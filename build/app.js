9497be870877dfcf3417549c2104ffd816a3bc17
Steven Marsh <stevenmarsh@Stevens-MacBook-Pro.local>
2025-12-11 18:38:16 -0500

    chore(config): add tsconfig file

---
+++ build/app.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let message = "Hello, TypeScript!";
+console.log(message);
+let message2 = 5;
+console.log(message2);
+const person = {
+    name: "Alice",
+    age: 30
+};
+console.log(person);
+//# sourceMappingURL=app.js.map
