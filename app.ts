ed61745f8eac9545019b9087338c185c4d31f2bb
Steven Marsh <stevenmarsh@Stevens-MacBook-Pro.local>
2025-12-11 12:06:49 -0500

    chore(practice): setup typescript playground project

---
+++ app.ts
@@ -0,0 +1,14 @@
+let message: string = "Hello, TypeScript!";
+console.log(message);
+let message2: number = 5;
+console.log(message2);
+
+interface Person {
+    name: string;
+    age: number;
+}
+const person: Person = {
+    name: "Alice",
+    age: 30
+};
+console.log(person);
